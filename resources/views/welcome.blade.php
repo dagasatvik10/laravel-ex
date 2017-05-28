977dda4bb1460092fa6c7fbd80a4ddf02832c4e9
Satvik Daga <dagasatvik@icloud.com>
2017-05-29 00:39:24 +0530

    just checking if push is working and build is being created...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Its game on</div>
             </div>
         </div>
     </body>
